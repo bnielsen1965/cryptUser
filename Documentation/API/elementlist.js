a4abd271f36305605b9dd5386fcf75bf123acd11
burnin <bnielsen1965@gmail.com>
2014-01-12 21:51:51 -0800

    Cleaning up master branch.

--- Documentation/API/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","BSN\\CryptUser\\CryptDataSource"],["c","BSN\\CryptUser\\CryptJSONSource"],["c","BSN\\CryptUser\\CryptMySQLSource"],["c","BSN\\CryptUser\\CryptUser"],["c","BSN\\CryptUser\\SSLKey"]];
